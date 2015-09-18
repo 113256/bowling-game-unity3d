ce0b8f756f239360896e74ab559f39c2a2be823b
jack chan <jackchan715@hotmail.com>
2015-09-18 14:44:15 -0700

    added cameraControl.cs

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Ball ball;
+	private Vector3 offset;
+	private GameObject pins;
+
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3(0,25,-150);
+		pins = GameObject.Find ("Pins");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if ball reaches the pins stop the camera from following it
+		if (ball.transform.position.z <= pins.transform.position.z) {
+			this.transform.position = ball.transform.position + offset;
+		} else {
+			print ("stop camera");
+		}
+	}
+}
